68b116e1fdb69a4eee74a35e049fc753789368db
Oleg <olegashepelev@gmail.com>
2024-02-05 22:09:02 +0600

    Change code on typescript

---
+++ lib/star-wars.ts
@@ -0,0 +1,60 @@
+// Модуль для работы с API Star Wars.
+// Все методы обращаются к стороннему сервису, запрашивают данные у него.
+// Методы асинхронны, они возвращают Promise.
+
+// Есть следующие методы:
+// starWars.searchCharacters(query),
+// starWars.searchPlanets(query),
+// starWars.searchSpecies(query).
+// starWars.getCharactersById(id),
+// starWars.getPlanetsById(id),
+// starWars.getSpeciesById(id)
+
+// Код ниже разбирать не нужно.
+// Всё, что вам необходимо знать: эти методы умеют получать данные и возвращают промисы.
+// Попробуйте запустить их в своем скрипте search.js.
+
+const starWars = {
+  // --- Search Methods ---
+
+  searchCharacters: (query: string) => {
+    return new Promise((resolve) => {
+      fetch(`https://swapi.dev/api/people/?search=${query}`)
+        .then((response) => response.json())
+        .then((characters) => resolve(characters))
+        .catch((err) => console.log("searchCharacters error: ", err));
+    });
+  },
+
+  searchPlanets: (query: string) => {
+    return new Promise((resolve) => {
+      fetch(`https://swapi.dev/api/planets/?search=${query}`)
+        .then((response) => response.json())
+        .then((planets) => resolve(planets))
+        .catch((err) => console.log("searchPlanets error: ", err));
+    });
+  },
+
+  searchSpecies: (query: string) => {
+    return new Promise((resolve) => {
+      fetch(`https://swapi.dev/api/species/?search=${query}`)
+        .then((response) => response.json())
+        .then((species) => resolve(species))
+        .catch((err) => console.log("searchSpecies error: ", err));
+    });
+  },
+
+  // --- Get By Id Methods ---
+
+  getCharactersById: async (id: number | string) =>
+    await (await fetch(`https://swapi.dev/api/people/${id}`)).json(),
+
+  getPlanetsById: async (id: number | string) =>
+    await (await fetch(`https://swapi.dev/api/planets/${id}`)).json(),
+
+  getSpeciesById: async (id: number | string) =>
+    await (await fetch(`https://swapi.dev/api/species/${id}`)).json(),
+
+  getFilmsById: async (id: number | string) =>
+    await (await fetch(`https://swapi.dev/api/films/${id}`)).json(),
+};
